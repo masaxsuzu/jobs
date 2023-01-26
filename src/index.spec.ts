564e94e7768853d770b4ec1c67fbc4261971ffac
masaxsuzu <masaxsuzu+github@gmail.com>
2023-01-27 00:47:44 +0900

    WIP

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,10 @@
-import { describe, test } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
+import { Job } from "bullmq";
+import { main } from ".";
 
 describe("index", () => {
-  test("index", () => {
+  test("index", async () => {
+    const handler = (job: Job, v: { color: string }) => {};
+    await main(handler);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log(`jobs`);
+import { exit } from "process";
+import { Job, Queue, Worker } from "bullmq";
+import IORedis from "ioredis";
+
+export async function main(handler: (job: Job, c: { color: string }) => void) {
+  const connection = new IORedis({ maxRetriesPerRequest: null });
+
+  // Reuse the ioredis instance
+  const queue = new Queue("queue", { connection });
+
+  await queue.add("queue", { color: "red" });
+
+  const worker = new Worker(
+    "queue",
+    async (job) => {
+      await job.updateProgress(100);
+      return job.data;
+    },
+    { connection }
+  );
+
+  worker.on("completed", handler);
+}
